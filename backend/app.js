669d368b052f2017efe46914b1fda3b43ee54f12
Marine <jeanningros.marine@gmail.com>
2021-06-24 16:32:14 +0200

    backend server and app setup

--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,23 @@
+const express =require("express");
+
+//body-parser
+const bodyParser = require('body-parser');
+
+//pour accéder au path de notre serveur
+const path = require('path');
+
+const app = express();
+
+//middleware body-parser (deprecated)
+app.use(bodyParser.json());
+
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+
 require('dotenv').config() 
+
+const apiRoutes = require("./routes/apiRoutes");
+app.use("/api", apiRoutes);
+
+//exporter cette application pour y accéder depuis les autres fichiers notamment le serveur
+module.exports = app;
--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,53 @@
-const express =require("express");
-const app = express();
-const db = require("./models");
-const PORT = process.env.port || 3000;
+//importer le package http natif de Node
+const http = require('http');
 
-app.use(express.urlencoded({extended:true}));
-app.use(express.json());
+//importer le package normalize-port
+const normalizePort = require('normalize-port');
+
+//importer l'application
+const app = require('./app');
+
+const db = require("./models");
+
+const port = normalizePort(process.env.PORT || '3000'); //par défaut en développement, sauf si pas dispo -> process.env.PORT
+
+app.set('port', port);
+
+//la fonction error Handler recherche les différentes erreurs et les gère de manière appropriée
+const errorHandler = error => {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+  const address = server.address();
+  const bind = typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges.');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use.');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+};
+
+//appeler app qui est une fonction qui sera exécutée via le serveur
+const server = http.createServer(app);
+
+//un écouteur d'évènement est également enregistré, consignant le port ou le canal nommé sur lequel le serveur s'exécute dans la console
+server.on('error', errorHandler);
+server.on('listening', () => {
+  const address = server.address();
+  const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
+  console.log('Listening on ' + bind);
+});
 
 db.sequelize.sync().then(() => {
-    app.listen(PORT, () => {
-        console.log(`listening on: http://localhost:${PORT}`);
+    app.listen(port, () => {
+        console.log(`listening on: http://localhost:${port}`);
     });
 });
+
