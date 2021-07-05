2593a47b112301b17ff66fb87cf32f7a9ec145d3
Marine <jeanningros.marine@gmail.com>
2021-07-05 18:06:24 +0200

    routes prepared for auth

--- backend/routes/post.js
+++ backend/routes/post.js
@@ -10,18 +10,23 @@ const authAdmin = require('../middlewares/authAdmin');
 
 
 //get all posts
-router.get("/", auth, postCtrl.findAllPosts);
+//router.get("/", auth, postCtrl.findAllPosts);
+router.get("/", postCtrl.findAllPosts);
 
 //get a single post
-router.get("/:id", auth, postCtrl.findOnePost)
+//router.get("/:id", auth, postCtrl.findOnePost)
+router.get("/:id", postCtrl.findOnePost)
 
 //post new post
-router.post("/", auth, postCtrl.createPost);
+//router.post("/", auth, postCtrl.createPost);
+router.post("/", postCtrl.createPost);
 
 //delete post
-router.delete("/:id", authAdmin, postCtrl.deletePost);
+//router.delete("/:id", authAdmin, postCtrl.deletePost);
+router.delete("/:id", postCtrl.deletePost);
 
 //edit a post
-router.put('/:id', auth, postCtrl.updatePost);
+//router.put('/:id', auth, postCtrl.updatePost);
+router.put('/:id', postCtrl.updatePost);
 
 module.exports = router;
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -11,7 +11,8 @@ const authAdmin = require('../middlewares/authAdmin');
 
 
 //get all users
-router.get("/", authAdmin, userCtrl.findAllUsers);
+//router.get("/", authAdmin, userCtrl.findAllUsers);
+router.get("/", userCtrl.findAllUsers);
 
 //get a single user
 router.get("/:id", userCtrl.findOneUser);
@@ -26,6 +27,7 @@ router.post("/", userCtrl.login);
 router.delete("/:id", authAdmin, userCtrl.deleteUser);
 
 //edit a user
-router.put('/:id', auth, userCtrl.updateUser);
+//router.put('/:id', auth, userCtrl.updateUser);
+router.put('/:id', userCtrl.updateUser);
 
 module.exports = router;
